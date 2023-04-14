f7cce5070af042422a165ec62823c0287218b48d
Martin Adler <mteu@mailbox.org>
2023-04-14 13:55:19 +0200

    [BUGFIX] Fix tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,30 @@
 <?php declare(strict_types=1);
 
+/*
+ * This file is part of the Symfony project "shrugify/yolo".
+ *
+ * Copyright (C) 2023 Martin Adler <mteu@mailbox.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\Filesystem\Filesystem;
 
 require dirname(__DIR__).'/vendor/autoload.php';
+
 DG\BypassFinals::enable();
 
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
-
-// Clear all caches
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 (new Filesystem())->remove(__DIR__.'/../var/cache/test');
